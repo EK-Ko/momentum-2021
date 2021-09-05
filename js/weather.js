bf9eb057064e803dd80116ae1237c3661cdeef20
EK-Ko <koeugenia@gmail.com>
2021-09-04 20:00:10 -0700

    fix more errors

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,25 @@
-const todaysWeather = document.querySelector("#weather");
+const API_KEY = "dbaac53539a07ce65a6d3013e330ee39";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("you live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        const name = data.name;
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+
+/*const todaysWeather = document.querySelector("#weather");
 
 const API_KEY = "dbaac53539a07ce65a6d3013e330ee39"
 const HIDDEN_CLASSNAME = "hidden";
@@ -7,12 +28,14 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.");
@@ -22,4 +45,4 @@ navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
 if (weather !== null && city !== null) {
     todaysWeather.classList.remove(HIDDEN_CLASSNAME);
-}
+}*/
