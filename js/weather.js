316770078433bc0d817e1e5e5642d29e30e5d42c
EK-Ko <koeugenia@gmail.com>
2021-09-04 19:08:38 -0700

    hide weather box

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,11 @@
+const todaysWeather = document.querySelector("#weather");
 
 const API_KEY = "dbaac53539a07ce65a6d3013e330ee39"
+const HIDDEN_CLASSNAME = "hidden";
 
-function onGeoOk(position){
-    const lat =position.coords.latitude;
-    const lon =position.coords.longitude;
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
@@ -12,8 +14,12 @@ function onGeoOk(position){
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
-function onGeoError(){
+function onGeoError() {
     alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+if (weather !== null && city !== null) {
+    todaysWeather.classList.remove(HIDDEN_CLASSNAME);
+}
